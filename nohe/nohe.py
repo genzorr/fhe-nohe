Fixed NOHE implementation (decoding of computation result) and socket connection, added server packet, changed flask form

nohe/nohe.py
@@ -35,9 +35,9 @@ class Coder():
         self.X = None
         self.Y = None
         self.p_box = None
-        self.blocks = []
-        self.not_positions = []
-        self.circular_shifts = []
+        self.blocks = None
+        self.not_positions = None
+        self.circular_shifts = None
 
         self.generate_keys()
 
@@ -132,34 +132,39 @@ class Coder():
 
     def permute_bits(self):
         # Generate and apply permutation box
-        self.p_box = self.generate_permutation_box(len(self.X))
+        if not self.p_box:
+            self.p_box = self.generate_permutation_box(len(self.X))
         self.X = list(map(self.X.__getitem__, self.p_box))
 
     def encrypt_blocks(self):
-        self.not_positions = []
         # Break X into blocks
         self.blocks = list(split_blocks(self.X, self.block_size))
 
         # Create secret blocks by applying RC4 to each block
-        cipher = ARC4.new(self.keys['d'])
-        for block in self.blocks:
-            secret_block = bytes_to_bits(cipher.encrypt(bits_to_bytes(block)))
-            # Extract positions of 1's
-            pos = [i for i,val in enumerate(secret_block) if val == 1]
-            self.not_positions.append(pos)
+        if not self.not_positions:
+            self.not_positions = []
+            cipher = ARC4.new(self.keys['d'])
+            for block in self.blocks:
+                secret_block = bytes_to_bits(cipher.encrypt(bits_to_bytes(block)))
+                # Extract positions of 1's
+                pos = [i for i,val in enumerate(secret_block) if val == 1]
+                self.not_positions.append(pos)
         # Apply secret NOT operations
         for n in range(len(self.blocks)):
             for i in self.not_positions[n]:
                 self.blocks[n][i] = 0 if self.blocks[n][i] == 1 else 1
 
         # Dynamic Key Selection Algorithm
-        # Generate delta-p-box and secret bank
-        delta_p_box = self.generate_permutation_box(len(self.blocks))
-        secret_bank = random.choices(range(0, self.block_size // 4 + 1), k=len(self.blocks))
-        # Create and apply circular shifts
-        self.circular_shifts = []
+        if not self.circular_shifts:
+            # Generate delta-p-box and secret bank
+            delta_p_box = self.generate_permutation_box(len(self.blocks))
+            secret_bank = random.choices(range(0, self.block_size // 4 + 1), k=len(self.blocks))
+            # Create and apply circular shifts
+            self.circular_shifts = []
+            for i in range(len(self.blocks)):
+                self.circular_shifts.append(secret_bank[delta_p_box[i]])
+
         for i in range(len(self.blocks)):
-            self.circular_shifts.append(secret_bank[delta_p_box[i]])
             self.blocks[i] = list(np.roll(self.blocks[i], self.circular_shifts[i]))
 
     def encode(self, X):
@@ -202,6 +207,37 @@ class Coder():
         # Output decoded text
         return bits_to_bytes(self.Y)
 
+    def decrypt_blocks_res(self, Z1, Z2):
+        blocks1 = list(split_blocks(Z1, self.block_size))
+        blocks2 = list(split_blocks(Z2, self.block_size))
+
+        # Apply inverse circular shifts
+        for i in range(len(blocks1)):
+            blocks1[i] = list(np.roll(blocks1[i], - self.circular_shifts[i]))
+            blocks2[i] = list(np.roll(blocks2[i], - self.circular_shifts[i]))
+
+        self.blocks = blocks1.copy()
+        # Tricky moment: we need to combine results of normal and homomorphic operations
+        # performed on encoded texts, so we choose normal bits from first result and bits from secret NOT positions
+        # from second. This operation may be omitted for XOR but not for AND!
+        for n in range(len(self.blocks)):
+            for i in self.not_positions[n]:
+                self.blocks[n][i] = blocks2[n][i]
+                # Apply secret NOT operations
+                self.blocks[n][i] = 0 if self.blocks[n][i] == 1 else 1
+
+    def decode_res(self, Z1, Z2):
+        Z1 = bytes_to_bits(Z1)
+        Z2 = bytes_to_bits(Z2)
+        # Dynamic blocks decryption
+        self.decrypt_blocks_res(Z1, Z2)
+        # Restore Y before permutation
+        self.Y = merge_blocks(self.blocks)
+        # Inverse permutation
+        self.inverse_permute_bits()
+        # Output decoded text
+        return bits_to_bytes(self.Y)
+
 class Operations(Enum):
     XOR = 1
     AND = 2
@@ -211,16 +247,23 @@ class Functions:
         self.X = X
         self.Y = Y
 
+    def plain_xor(self):
+        p_xor = bytearray((a ^ b) for (a, b) in zip(self.X, self.Y))
+        return bytes(p_xor)
+
+    def plain_and(self):
+        p_and = bytes((a & b) for (a, b) in zip(self.X, self.Y))
+        return bytes(p_and)
+
     def nohe_xor(self):
         homomorphic_xor = bytearray((a ^ b) for (a, b) in zip(self.X, self.Y))
-        for index in range(len(homomorphic_xor)):
-            homomorphic_xor[index] ^= 0xFF
-        homomorphic_xor = bytes(homomorphic_xor)
-        return homomorphic_xor
+        bi = [1 if x == 0 else 0 for x in bytes_to_bits(homomorphic_xor)]
+        homomorphic_xor = bits_to_bytes(bi)
+        return bytes(homomorphic_xor)
 
     def nohe_and(self):
         homomorphic_and = bytes((a | b) for (a, b) in zip(self.X, self.Y))
-        return homomorphic_and
+        return bytes(homomorphic_and)
 
 '''
 A small example to show how algorithm works: